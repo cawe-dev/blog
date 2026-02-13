2ea2986b255c2a283983c473011eb09c2a3bc0b5
Cawe Alves <cawe.wewe04@gmail.com>
2026-02-13 16:39:30 -0300

    fix(post): save post author

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -5,10 +5,18 @@ namespace App\Filament\Resources\Posts\Pages;
 use App\Filament\Resources\Posts\PostResource;
 use App\Services\PostReferenceParser;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = Auth::id();
+
+        return $data;
+    }
 
     protected function afterCreate(): void
     {
