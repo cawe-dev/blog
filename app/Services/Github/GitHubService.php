7b37b4a91b25ba4b99f4a6bc245dce98423cb1d2
Cawe Alves <cawe.wewe04@gmail.com>
2026-02-14 18:42:14 -0300

    feat(github): add cache on request

--- app/Services/Github/GitHubService.php
+++ app/Services/Github/GitHubService.php
@@ -6,24 +6,28 @@ use App\Exceptions\GithubRequestException;
 use App\Facade\GithubServiceFacade as GitHub;
 use App\Support\Github\GithubCommit;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class GitHubService
 {
-    public static function getCommitsByBranch(string $branch, int $page = 1, int $perPage = 10): Collection
+
+    public function getCommitsByBranch(string $branch, int $page = 1, int $perPage = 10): Collection
     {
-        $response =  Github::get('commits', [
-            'sha' => $branch,
-            '&page' => $page,
-            '&per_page' => $perPage
-        ]);
+        return Cache::remember("github_commits_{$branch}", 300, function () use ($branch, $page, $perPage) {
+            $response =  Github::get('commits', [
+                'sha' => $branch,
+                '&page' => $page,
+                '&per_page' => $perPage
+            ]);
 
-        if ($response->failed()) {
-            throw GithubRequestException::fromResponse($response);
-        }
+            if ($response->failed()) {
+                throw GithubRequestException::fromResponse($response);
+            }
 
-        return collect($response->json())->map(
-            fn(array $commit) =>
-            GithubCommit::fromArray($commit)
-        );
+            return collect($response->json())->map(
+                fn(array $commit) =>
+                GithubCommit::fromArray($commit)
+            );
+        });
     }
 }
