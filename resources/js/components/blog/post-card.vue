c05452b120fc095da75cf467837f2a3dd0585042
Cawe Alves <cawe.wewe04@gmail.com>
2026-02-15 20:55:46 -0300

    style(post): standard postcard size

--- resources/js/components/blog/post-card.vue
+++ resources/js/components/blog/post-card.vue
@@ -25,13 +25,14 @@ const toggleSpoiler = () => {
 
 <template>
     <UBlogPosts :orientation="viewMode === 'list' ? 'vertical' : 'vertical'">
-        <UBlogPost :to="computedPath" :image="post.thumbnail" :class="viewMode === 'list' ? 'px-4' : ''" :ui="{
-            image: 'object-cover',
-            title: 'text-xl font-bold text-default mb-2 group-hover:text-primary transition-colors',
-            description: 'text-muted text-base',
-            date: 'text-xs text-muted mt-4',
-            authors: 'hidden',
-        }">
+        <UBlogPost class="h-full" :to="computedPath" :image="post.thumbnail" :class="viewMode === 'list' ? 'px-4' : ''"
+            :ui="{
+                image: 'object-cover',
+                title: 'text-xl font-bold text-default mb-2 group-hover:text-primary transition-colors',
+                description: 'text-muted text-base',
+                date: 'text-xs text-muted mt-4',
+                authors: 'hidden',
+            }">
             <template #badge>
                 <UBadge v-if="post.pinned_at" color="secundary" variant="subtle">
                     <UIcon name="i-lucide-pin" class="w-4 h-4 text-toned" />
